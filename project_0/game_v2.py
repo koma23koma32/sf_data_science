3d076fa369d152472c574e108a445c72fcdffe92
KO MA <koma23koma32@gmail.com>
2021-09-14 10:34:26 +0300

    edit ipynb

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -35,5 +35,6 @@ def score_game(random_predict) -> int:
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-
-score_game(random_predict)
+if __name__ == '__main__':
+    
+    score_game(random_predict)
